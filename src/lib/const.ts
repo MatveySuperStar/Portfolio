439b974bd890cad87a136d6a42805bbce571c705
MatveySuperStar <sergeev2000matvey@mail.ru>
2024-07-18 16:55:33 +0300

    init project

---
+++ src/lib/const.ts
@@ -0,0 +1,69 @@
+import balmy from "@/public/projects/balmy.png";
+import baunty from "@/public/projects/baunty.png";
+import royal from "@/public/projects/royal.webp";
+import webSpace from "@/public/projects/web-space.png";
+import masterMod from "@/public/projects/master-mod.png";
+import prokatchik from "@/public/projects/prokatchik.png";
+import aef from "@/public/projects/aef.png";
+import sauletta from "@/public/projects/sauletta.png";
+import monument from "@/public/projects/monument.png";
+import amp from "@/public/projects/amp.png";
+import remontStolin from "@/public/projects/remont-stolin.png";
+import luxHome from "@/public/projects/lux-home.png";
+import filmPro from "@/public/projects/film-pro.png";
+import myvision from "@/public/projects/my-vision.png";
+
+export const projects = [
+  {
+    title: "remont-stolin",
+    image: remontStolin,
+    href: "https://vercel.com/matveys-projects-69b106dd/remont-stolin",
+  },
+  {
+    title: "monument",
+    image: monument,
+    href: "https://monuments-flame.vercel.app/",
+  },
+  { title: "sauletta", image: sauletta, href: "https://sauletta.vercel.app/" },
+  { title: "balmy", image: balmy, href: "https://balmy.by/" },
+  { title: "myvision", image: myvision, href: "https://my-vision.vercel.app/" },
+  { title: "film pro", image: filmPro, href: "https://film-pro.vercel.app/" },
+
+  {
+    title: "lux home",
+    image: luxHome,
+    href: "https://vercel.com/matveys-projects-69b106dd/lux-home",
+  },
+  {
+    title: "amp",
+    image: amp,
+    href: "https://vercel.com/matveys-projects-69b106dd/amp",
+  },
+
+  {
+    title: "AEF",
+    image: aef,
+    href: "https://vercel.com/matveys-projects-69b106dd/alfa-euro-stroy/9Bs74ykYjpjgRdeggFrZuPZ75fhZ",
+  },
+  {
+    title: "prokatchik",
+    image: prokatchik,
+    href: "https://prokatchik.vercel.app/",
+  },
+  { title: "web space", image: webSpace, href: "https://web-space.by" },
+  {
+    title: "master mod",
+    image: masterMod,
+    href: "https://master-mod.vercel.app/",
+  },
+  {
+    title: "bauntyspa",
+    image: baunty,
+    href: "https://bauntyspa.by/",
+  },
+  {
+    title: "royalspa",
+    image: royal,
+    href: "http://178.172.244.147:3000/",
+  },
+];
